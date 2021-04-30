6909b66abd7342105b024ee966f72362a4450571
Edwin Takahashi <egao@outlook.com>
2021-04-30 16:48:11 -0700

    Implement Playwright screenshot capture on a new branch.
    - move the MediaHelper file inside the calypso-e2e package.
    - write a new afterEach hook for screenshot capture.

--- test/e2e/specs-playwright/wp-log-in-out-spec.js
+++ test/e2e/specs-playwright/wp-log-in-out-spec.js
@@ -15,7 +15,6 @@ const mochaTimeOut = config.get( 'mochaTimeoutMS' );
 
 describe( `Auth Screen @canary @parallel`, function () {
 	this.timeout( mochaTimeOut );
-
 	// Page represents a tab in a browser.
 	// Test steps interact with the page to execute its instructions.
 	let page;
@@ -23,8 +22,9 @@ describe( `Auth Screen @canary @parallel`, function () {
 	// Open a new page (tab) within the BrowserContext.
 	// This is akin to the following call in the Selenium test:
 	// driver = await driverManager.startBrowser();
-	before( 'Open new test tab', async function () {
+	beforeEach( 'Open new test tab', async function () {
 		page = await this.browserContext.newPage();
+		this.currentTest.page = page;
 	} );
 
 	describe( 'Loading the log-in page', function () {
